54225d3a0d14e39232334bc41058cc01ee6046d3
admin <ajaykumar143ece@gmail.com>
2023-07-26 15:53:25 +0530

    addition

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import streamlit as st
 
-st.write("Hello World")
-st.button('Create a new app')
-st.checkbox('I agree')
-st.radio('Pick one', ['cats', 'dogs'])
-st.selectbox('Pick one', ['cats', 'dogs'])
-st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
-st.slider('Pick a number', 0, 100)
-st.select_slider('Pick a size', ['S', 'M', 'L'])
-st.text_input('First name')
+a = st.number_input("Enter First number")
+b = st.number_input("Enter Second Number")
 
-st.write(['st', 'is <', 3])
+if st.button(" Addition "):
+    st.write(a+b)
