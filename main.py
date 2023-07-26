fb70c4a0a7ebd237af67d481c29f8a16ac5d4224
admin <ajaykumar143ece@gmail.com>
2023-07-26 15:35:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write("Hello World")
+st.button('Create a new app')
+st.checkbox('I agree')
+st.radio('Pick one', ['cats', 'dogs'])
+st.selectbox('Pick one', ['cats', 'dogs'])
+st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
+st.slider('Pick a number', 0, 100)
+st.select_slider('Pick a size', ['S', 'M', 'L'])
+st.text_input('First name')
+
+st.write(['st', 'is <', 3]) # see *
